347b2af338a604578c23f8922460e61bef2d16ed
eddyzera <esilva644@gmail.com>
2023-02-16 16:50:50 -0300

    feat: use Header in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app'
+import { Header } from '@/components'
 import '../styles/global.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   )
 }
